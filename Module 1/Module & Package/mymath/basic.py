33a69eece508cb6414f5c9bd6d8dc127845ab05f
OxwardInc <divineogbonda@yahoo.com>
2025-04-27 22:50:53 +0100

    update

---
+++ Module 1/Module & Package/mymath/basic.py
@@ -0,0 +1,18 @@
+""" Basic Math program """
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
+
+def add(a, b):
+    """
+    This funtion returns the sum of two numbers 
+    """
+    return a + b
---
+++ Module 1/Unit testing/test/mymodule.py
@@ -0,0 +1,6 @@
+""" This module checks the sum of two numbers """
+def add(a, b):
+    """
+    This funtion returns the sum of the given numbers
+    """
+    return a + b
